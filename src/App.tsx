3965e82baff09fd9f0e0b5418ba936b71ce10ba7
shoshana.huri <shoshana.huri@infinitylabs.co.il>
2024-07-02 12:02:34 +0300

    about me integrate with app

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@
 import "./App.css";
 
 import Main from "./components/Main";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
     <>
       <Main />
+      <AboutMe />
     </>
   );
 }
