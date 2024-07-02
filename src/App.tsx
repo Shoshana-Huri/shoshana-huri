55b45e00177aab735c555c4b0b646ca8190e9587
shoshana.huri <shoshana.huri@infinitylabs.co.il>
2024-07-02 12:13:52 +0300

    contact

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Main from "./components/Main";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <AboutMe />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
